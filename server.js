cdcfc2cf012a45536f9992a589644c84a7296855
GhaisaniCan <ghaisanrifqikamiel@mail.ugm.ac.id>
2026-09-26 12:30:05 +0700

    feat: implementasi rute jenis sampah dan integrasi autentikasi

--- server.js
+++ server.js
@@ -10,5 +10,10 @@ app.use(express.json())
 const authController = require("./controllers/authController")
 app.use("/auth", authController)
 
+//-------------------- idk what im doing dont mind me ---------------------
+const jenisSampahController = require("./controllers/jenisSampahController")
+app.use("/jenis-sampah", jenisSampahController)
+//-------------------- idk what im doing dont mind me ---------------------
+
 app.get("/ping", (req, res) => res.json({ "data": "pong" }))
 app.listen(5001, () => console.log("Server is up and running"))
